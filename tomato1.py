55c8b873e42baa05b5ae863097e1c6268217eeed
JCVis <jcvisschers@gmail.com>
2020-12-16 16:52:44 +0100

    Update tomato1.py

--- tomato1.py
+++ tomato1.py
@@ -14,7 +14,7 @@ dev = btle.Peripheral("10:52:1C:5D:68:CA")
 chars = dev.getCharacteristics()
 
 variables = [['f22','Temperature'],['f23','Pressure'],['f24','Air Humidity'], ['f25','Soil Humidity']]
-values  = []
+values  = ''
 for char in chars:
     hand = char.getHandle()
     UUID = str(char.uuid)
@@ -24,7 +24,7 @@ for char in chars:
             val = dev.readCharacteristic(hand).decode('utf-8')
             values.append(val)
             print(val)
-            
+print(values)
 f = open('Tomato1.txt', 'a+')
-f.write(values)
+f.write()
 f.close()
